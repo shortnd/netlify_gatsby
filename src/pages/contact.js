a489a4e53825a7a6f1b2d57f5c907e32ca28aa0d
Collin OConnell <ococncol@gmail.com>
2020-06-19 11:50:38 -0400

    added formik and yup

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,15 +1,32 @@
 import React from 'react'
 import Layout from '../components/layout'
 
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
+
 export default function Contact() {
+    const formik = useFormik({
+        initialValues: {
+            email: ''
+        },
+        validationSchema: Yup.object({
+            email: Yup.string().email('Please provide a valid email').required('Required')
+        }),
+        validateOnMount: true
+    })
     return (
         <Layout>
             <h2>Contact</h2>
             <form data-netlify="true" name="contact">
                 <input type="hidden" name="form-name" name="contact" />
                 <label htmlFor="email">Email</label>
-                <input type="email" name="email" id="email" />
-                <button type="submit">submit</button>
+                <input type="email" name="email" id="email" {...formik.getFieldProps('email')} required />
+                {(formik.touched.email && formik.errors.email) ? (
+                    <>
+                        {formik.errors.email}
+                    </>
+                ) : null}
+                <button type="submit" disabled={!formik.isValid}>submit</button>
             </form>
         </Layout>
     )
