ce107a793c2caf8dab0ce96898f00b7e1bdb2e68
Collin OConnell <ococncol@gmail.com>
2020-06-19 10:25:53 -0400

    removed action

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
-    <form method="post" data-netlify="true" data-netlify-recaptcha="true" netlify-honeypot="bot-field" name="form-test" action="/success">
+    <form method="post" data-netlify="true" data-netlify-recaptcha="true" netlify-honeypot="bot-field" name="form-test">
       <input type="hidden" name="form-name" value="contact" />
         <p class="hidden">
           <label>Don’t fill this out if you're human: <input name="bot-field" type="hidden" /></label>
