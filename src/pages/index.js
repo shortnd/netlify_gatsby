e428dbf86f605ae66cd6daaf05452e77d078da9c
Collin OConnell <ococncol@gmail.com>
2020-06-19 09:53:12 -0400

    adding filtering

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,11 +9,15 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
-    <form method="post" data-netlify="true" name="form-test" action="/success">
+    <form method="post" data-netlify="true" data-netlify-recaptcha="true" netlify-honeypot="bot-field" name="form-test" action="/success">
+        <p class="hidden">
+          <label>Don’t fill this out if you're human: <input name="bot-field" /></label>
+        </p>
       <label htmlFor="firstName">
         First Name
         <input type="text" name="firstName" id="firstName" placeholder="First Name" required/>
       </label>
+        <div data-netlify-recaptcha="true"></div>
       <div>
         <button type="submit">Submit</button>
       </div>
